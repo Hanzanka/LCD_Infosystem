db647ddb8872fcfea0a217103b9b94fca4440e73
Ville Einiö <ville.einio@gmail.com>
2022-01-30 00:09:22 +0200

    Refactoring

--- internet_speedtest/networkmonitor.py
+++ internet_speedtest/networkmonitor.py
@@ -1,40 +1,37 @@
+from enum import Enum
+import logging
 from time import sleep
-from multiprocessing import Pipe
+import asyncio
 from threading import Thread
 
 
-class NetworkMonitor:
-    def __init__(
-        self, update_interval=2, rounding=2, iface="eth0", direction="rx"
-    ) -> None:
-        self.__update_interval = update_interval
-        self.__rounding = rounding
-        
-        self.__direction = direction
-        self.__direction_dict = {"rx": "tx", "tx": "rx"}
+class BandwidthMonitor:
+    def __init__(self, iface="eth0") -> None:
+
+        self.__update_interval = 2
+        self.__rounding = 2
         self.__iface = iface
-        
-        self.__commands = {"switch": self.__switch_direction}
-        
-        self.__monitor_pipe = None
-        self.__controller_pipe = None
+        self.__direction = {
+            BandwidthMonitorState.DOWNLOAD: "rx",
+            BandwidthMonitorState.UPLOAD: "tx",
+        }
 
+        self.__event_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.__event_loop)
+        self.__wait_event = asyncio.Event()
+        
+        self.__state = BandwidthMonitorState.OFF
+        
         self.__monitor_thread = None
-        self.__controller_thread = None
 
-        self.name = 'networkmonitor.py'
+        self.__logger = logging.getLogger()
+        self.__logger.setLevel(logging.INFO)
+
+        self.__call_on_update = None
 
     def __monitor_bandwidth(self) -> None:
-        while True:
-            speed = self.__transfer_rate()
-            try:
-                self.__monitor_pipe.send(speed)
-            except OSError:
-                print(f'Closing bandwidth monitor on {self.name}')
-                break
-            except BrokenPipeError:
-                print(f'Closing bandwidth monitor on {self.name}')
-                break
+        self.__call_on_update(self.__transfer_rate())
+        asyncio.get_event_loop().call_soon(self.__monitor_bandwidth)
 
     def __get_bytes(self, direction: str):
         with open(
@@ -43,55 +40,58 @@ class NetworkMonitor:
             data = file.read()
             return int(data)
 
-    def __transfer_rate(self) -> tuple:
+    def __transfer_rate(self) -> list:
+        if self.__state == BandwidthMonitorState.READY or self.__state == BandwidthMonitorState.OFF:
+            return []
+        direction = self.__direction[self.__state]
         results = []
         for _ in range(5):
-            direction = self.__direction
             result_1 = self.__get_bytes(direction)
             sleep(1 / self.__update_interval / 5)
             result_2 = self.__get_bytes(direction)
-            results.append(
-                self.__convert_to_megabits(result_1, result_2)
-                * 5
-                * self.__update_interval
-            )
-        return round(sum(results) / len(results), self.__rounding)
+            results.append((result_2 - result_1) * 5 * self.__update_interval)
+        return self.__convert_to_megabits(
+            round(sum(results) / len(results), self.__rounding)
+        )
 
-    def __convert_to_megabits(self, result_1, result_2) -> float:
-        bits = 8 * (result_2 - result_1)
+    async def __wait_until_ready(self) -> None:
+        await self.__wait_event.wait()
+
+    def __convert_to_megabits(self, result_in_B) -> float:
+        bits = 8 * result_in_B
         megabits = bits / (1024 ** 2)
         return megabits
 
-    def start(self, monitor_pipe: Pipe, controller_pipe: Pipe):
-        
-        self.__controller_pipe = controller_pipe
-        self.__monitor_pipe = monitor_pipe
+    def __start_loop(self) -> None:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        self.__wait_event = asyncio.Event()
+        loop.call_soon(self.__monitor_bandwidth)
+        loop.run_until_complete(self.__wait_until_ready())
 
-        self.__monitor_thread = Thread(target=self.__monitor_bandwidth, args=[])
-        self.__controller_thread = Thread(target=self.__controller, args=[])
-
+    def start(self, function: callable):
+        self.__call_on_update = function
+        self.__monitor_thread = Thread(target=self.__start_loop)
         self.__monitor_thread.start()
-        self.__controller_thread.start()
 
-    def __switch_direction(self):
-        self.__direction = self.__direction_dict[self.__direction]
+    def reset(self) -> None:
+        self.__state == BandwidthMonitorState.OFF
 
-    def __controller(self) -> None:
-        while True:
-            command = self.__controller_pipe.recv()
-            if command != "stop" and command in self.__commands:
-                self.__commands[command]()
-            elif command == "stop":
-                print(f"Closing controller on {self.name}")
-                self.__monitor_pipe.close()
-                self.__controller_pipe.close()
-                break
-        
-        self.__monitor_thread.join()
-        
-        if not self.__monitor_thread.is_alive():
-            print(f'Bandwidth monitor on {self.name} is closed')
-        else:
-            print(f'Bandwidth monitor on {self.name} is still alive')
-            
-        print(f'Controller on {self.name} is closed')
+    def set_state(self, state) -> None:
+        self.__state = state
+        if state == BandwidthMonitorState.READY:
+            self.__wait_event.set()
+
+            self.__monitor_thread.join(timeout=10)
+
+            if not self.__monitor_thread.is_alive():
+                self.__logger.info("Monitor thread is closed")
+            else:
+                self.__logger.info("Monitor thread is still alive")
+
+
+class BandwidthMonitorState(Enum):
+    OFF = "off"
+    DOWNLOAD = "download"
+    UPLOAD = "upload"
+    READY = "ready"
